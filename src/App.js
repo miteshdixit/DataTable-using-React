10b87831d6f312507d72bea8ede8292ca52f646d
mitesh dixit <miteshdixit741@gmail.com>
2024-07-05 01:16:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+/* eslint-disable no-unused-vars */
 import './App.css';
+import { useState } from 'react';
+import Papa from 'papaparse';
+import 'datatables.net-dt';
+import Filter from './Filter';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [loading, setLoad] = useState(false);
+    const [filterdta , setFilterDta] = useState([])
+
+    const load = async() => {
+    
+        await fetch("/Sample-Data-Screener.csv")
+            .then(response => response.text())
+            .then(csvText => {
+                Papa.parse(csvText, {
+                    header: true,
+                    complete: (results) => {
+                        setFilterDta(results.data)
+
+                    }
+                });
+            });
+            setLoad(true);
+    };
+
+    return (
+        <div className='px-5 mt-5 min-vh-100 justify-content-center align-items-center'>
+{ loading ?
+
+(
+  <Filter data={filterdta} />
+)
+: 
+(
+<div className='container-fluid d-flex min-vh-100 justify-content-center align-items-center ' >
+<button onClick={load} className=' btn btn-warning m-2'>Load</button>
+<p className='justify-baseline'>click button to load data-table</p>
+</div>
+)
+}
+            
+            
+        </div>
+    );
 }
 
 export default App;
